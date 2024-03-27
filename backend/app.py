13d677555401f4f33d56314a1903368b695e8c63
Azka Asim <i202478@nu.edu.pk>
2024-03-27 17:46:53 +0500

    task 5 first commit

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://db:27017/mlop_task_5_i202478'
+mongo = PyMongo(app)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json 
+    name = data.get('name')
+    email = data.get('email')
+
+    if name and email:
+        # Insert data into MongoDB
+        mongo.db.users.insert_one({'name': name, 'email': email})
+        return jsonify({'message': 'Data inserted successfully'}), 200
+    else:
+        return jsonify({'error': 'Name and email are required'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
